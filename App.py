f6a11f5f0a568d825b0d0638c9ae454a9fdbe857
comrane <vladimir.dolgov@yahoo.com>
2020-03-30 02:09:50 +0300

    Create a __main__ entry point - App #2

--- App.py
+++ App.py
@@ -1,12 +1,21 @@
 import tkinter as tk
-from Dictionary import AutocompleteEntry
+# from Dictionary import AutocompleteEntry
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        auto_entry = AutocompleteEntry(self)
-        auto_entry.grid()
+        conn = sqlite3.connect('dictionary.db')
+        conn.row_factory = lambda cursor, row: row[0]
+        c = conn.cursor()
+        test_list = c.execute("SELECT eng FROM D1").fetchall()
+        conn.commit()
+        conn.close()
+
+        entry = AutocompleteEntry(root)
+        entry.set_completion_list(test_list)
+        entry.pack()
+        entry.focus_set()
 
 
 if __name__ == "__main__":
